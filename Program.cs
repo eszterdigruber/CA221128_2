38cf7684532efb9a062a15dca22c7a7340cd5bde
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2022-11-28 14:39:55 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -2,6 +2,48 @@
 
 //feladatlap f1
 Dolg01();
+Dolg02();
+
+void Dolg02()
+{
+    Console.Write("maximálisan megengedett sebesség (kmph): ");
+    int limit = int.Parse(Console.ReadLine());
+
+    if (limit <= 0 || limit > 50)
+    {
+        Console.WriteLine("Ez a program csak 50 kmph sebességkorlátig tudja meghatározni a bírságot.");
+    }
+    else
+    {
+        Console.Write("tényleges sebesség (kmph) : ");
+        int aktSeb = int.Parse(Console.ReadLine());
+        if (aktSeb <= limit)
+        {
+            Console.WriteLine("nem lépted túl a sebességkorlátozast, nincs büntetés.");
+        }
+        else
+        {
+            int tullepes = aktSeb - limit;
+            if (tullepes <= 15)
+            {
+                Console.WriteLine("a túllépés tűréshatárán belül van, nincs büntetés, de legyél óvatosabb!");
+            }
+            else
+            {
+                int birsag = 0;
+                if (tullepes < 25) birsag = 30000;
+                else if (tullepes < 35) birsag = 45000;
+                else if (tullepes < 45) birsag = 60000;
+                else if (tullepes < 55) birsag = 90000;
+                else if (tullepes < 65) birsag = 130000;
+                else if (tullepes < 75) birsag = 200000;
+                else                    birsag = 300000;
+
+                Console.WriteLine($"a megengedett sebességet {tullepes} kmphval haladtad meg a büntetés {birsag} Ft");
+            }
+        }
+    }
+}
 
 void Dolg01()
 {
