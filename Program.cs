f8b6a10fbeb04b05607724c609297702178e4b51
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2022-11-28 14:21:49 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -5,5 +5,23 @@ Dolg01();
 
 void Dolg01()
 {
-    
+    Console.Write("karakterlánc: ");
+    string str = Console.ReadLine();
+    int rndNum = rnd.Next(10);
+    Console.WriteLine($"a generált szám: {rndNum}");
+
+    if (rndNum % 2 == 0)
+    {
+        for (int i = 0; i < rndNum; i++)
+        {
+            Console.Write($"{str} ");
+        }
+    }
+    else
+    {
+        for (int i = 0; i < rndNum; i++)
+        {
+            Console.WriteLine(str);
+        }
+    }
 }
